b46c9b39e5bf02e543211707bb42993dd90be102
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-11-13 23:39:49 -0600

    better css

--- name-of-your-dog-app-project/src/components/Header.js
+++ name-of-your-dog-app-project/src/components/Header.js
@@ -4,9 +4,9 @@ function Header({setView}) {
   return (
     <div className='header'>
         <h1>Dogs!</h1>
-        <nav>
+        <nav className='links'>
             <h3 onClick={() => {setView('Breed List')}}>Breed List</h3>
-            <h3 onClick={() => {setView('Search Dogs')}}>Search Dogs</h3>
+            <h3 onClick={() => {setView('Search Dogs')}} className='search'>Search Dogs</h3>
         </nav>
     </div>
   )
